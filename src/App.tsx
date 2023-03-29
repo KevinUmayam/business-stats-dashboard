958293ea6edf7ae5c33cfd5877bceabf31681940
Kevin Umayam <kevin.m.umayam@gmail.com>
2023-03-29 13:53:00 -0700

    added themes and imported them to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { createTheme } from "@mui/material/styles"
+import { useMemo } from "react"
+import { themeSettings } from "./theme"
 
 
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
   return (
     <div className="App">
-    
+    <ThemeProvider theme={theme}> 
+      <CssBaseline/>
+      hi
+    </ThemeProvider>
     </div>
   )
 }
