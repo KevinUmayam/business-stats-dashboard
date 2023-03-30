02a5cdf190c56bd5762a7792944b73e092094ebf
Kevin Umayam <kevin.m.umayam@gmail.com>
2023-03-29 18:58:18 -0700

    changes

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,7 +1,8 @@
 import { useMediaQuery } from "@mui/material";
 import {Box, Typography, useTheme} from "@mui/material"
-
-type Props = {};
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 
 const gridTemplateLargeScreens = `
 "a b c"
@@ -48,7 +49,7 @@ const gridTemplateSmallScreens = `
 "j"
 `;
 // sx={{fontSize: "28px"}}
-const Dashboard = (props: Props) => {
+const Dashboard = () => {
     const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)")
     const { palette } = useTheme();
   return (
@@ -67,16 +68,10 @@ const Dashboard = (props: Props) => {
         }
     }
     >
-        <Box bgcolor="#fff" gridArea="a"></Box>
-        <Box bgcolor="#fff" gridArea="b"></Box>
-        <Box bgcolor="#fff" gridArea="c"></Box>
-        <Box bgcolor="#fff" gridArea="d"></Box>
-        <Box bgcolor="#fff" gridArea="e"></Box>
-        <Box bgcolor="#fff" gridArea="f"></Box>
-        <Box bgcolor="#fff" gridArea="g"></Box>
-        <Box bgcolor="#fff" gridArea="h"></Box>
-        <Box bgcolor="#fff" gridArea="i"></Box>
-        <Box bgcolor="#fff" gridArea="j"></Box>
+
+        <Row1/>
+        <Row2/>
+        <Row3/>
     </Box>
   )
 }
