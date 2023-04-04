4ce3860e738a632cf93d375e985ca08bd3716bd0
Kevin Umayam <kevin.m.umayam@gmail.com>
2023-04-04 11:18:26 -0700

    worked relentlessly at trying to deploy my backend

--- server /index.js
+++ server /index.js
@@ -5,10 +5,13 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import kpiRoutes from "./routes/kpi.js"
-import KPI from "./models/KPI.js"
-import { kpis } from "./data/data.js"
-
+import kpiRoutes from "./routes/kpi.js";
+import productRoutes from "./routes/product.js";
+import transactionRoutes from "./routes/transaction.js";
+import KPI from "./models/KPI.js";
+import Product from "./models/Product.js";
+import Transaction from "./models/Transaction.js";
+import { kpis, products, transactions } from "./data/data.js";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -21,8 +24,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-/* ROUTES  */
+/* ROUTES */
 app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
@@ -33,14 +38,8 @@ mongoose
     })
     .then(async () => {
         app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-        await mongoose.connection.db.dropDatabase();
-        KPI.insertMany(kpis)
-        // do not do this when working with a real database right now this is just deleting seed data.
 
         /* ADD DATA ONE TIME ONLY OR AS NEEDED */
-        // await mongoose.connection.db.dropDatabase();
-        // KPI.insertMany(kpis);
-        // Product.insertMany(products);
-        // Transaction.insertMany(transactions);
+
     })
     .catch((error) => console.log(`${error} did not connect`));
